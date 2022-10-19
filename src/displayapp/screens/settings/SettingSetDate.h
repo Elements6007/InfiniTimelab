746653402ed205fe3086be3fe38de2e2fa828c57
Elements6007 <elements6007@gmail.com>
2022-10-18 22:14:32 -0400

    Update SettingSetDate.h

--- src/displayapp/screens/settings/SettingSetDate.h
+++ src/displayapp/screens/settings/SettingSetDate.h
@@ -15,10 +15,14 @@ namespace Pinetime {
       public:
         SettingSetDate(DisplayApp* app, Pinetime::Controllers::DateTime& dateTimeController);
         ~SettingSetDate() override;
-
+        
         void HandleButtonPress();
         void CheckDay();
+        
+        void HandleButtonPress(lv_obj_t *object, lv_event_t event);
 
+        bool OnTouchEvent(TouchEvents event) override;
+        
       private:
         Controllers::DateTime& dateTimeController;
 
